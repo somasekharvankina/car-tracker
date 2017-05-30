ad48a3005e9653c3dfa6a824095ba1733e6479cc
somasekharvankina <somasekharvankina@gmail.com>
2017-05-30 17:31:03 -0400

    refactor(moduel-api): modify line spaces, organise files

--- api/src/main/java/car/tracker/service/readingsServiceImpl.java
+++ api/src/main/java/car/tracker/service/readingsServiceImpl.java
@@ -31,7 +31,7 @@ public class readingsServiceImpl implements readingsService {
 
     @Override
     public readings create(readings read) {
-        if(read!= null) {
+        if (read != null) {
             String message;
             Alert obj = new Alert();
             tiress tirevalue = read.getTires();
@@ -39,26 +39,33 @@ public class readingsServiceImpl implements readingsService {
             if (tirevalue.getFrontLeft() < 32 || tirevalue.getFrontLeft() > 36 || tirevalue.getFrontRight() < 32 || tirevalue.getFrontRight() > 36
                     || tirevalue.getRearLeft() < 32 || tirevalue.getRearLeft() > 36 || tirevalue.getRearRight() < 32 || tirevalue.getRearRight() > 36) {
 
-                repository.CreateAlert(read.getVin(),"Tire Pressure is Low","Low");
-
-            if (read.getCheckEngineLightOn() || read.getEngineCoolantLow()) {
-                
-                repository.CreateAlert(read.getVin(),"Soon attention needed for ","Low");
-
+                repository.CreateAlert(read.getVin(), "Tire Pressure is Low", "Low");
             }
 
-            return repository.create(tirevalue,read);
+                if (read.getCheckEngineLightOn() || read.getEngineCoolantLow()) {
+
+                    repository.CreateAlert(read.getVin(), "Soon attention needed for ", "Low");
+
+                }
+
+                return repository.create(tirevalue, read);
+
+            } else
+                throw new BadRequestException("No data found in " + read);
         }
 
-        else throw new BadRequestException("No data found in " + read );
-    }
 
     @Override
     public void delete(String vin) {
 
         List<readings> result = repository.findByVin(vin);
+
         if(result!= null){
             repository.delete(result.get(0));
+            }
+        else{
+        throw  new BadRequestException("no data found on the Vin" + vin);
+           }
         }
     }
-}
+
